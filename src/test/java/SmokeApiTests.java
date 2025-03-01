84810d879830e5da9decca810e59989e338e039a
KuzNick <n9858508501@gmail.com>
2025-03-01 17:48:51 +0300

    add second APItest

--- src/test/java/SmokeApiTests.java
+++ src/test/java/SmokeApiTests.java
@@ -1,11 +1,16 @@
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static io.restassured.RestAssured.given;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SmokeApiTests {
     public static final String BASE_URL = "https://petstore.swagger.io/v2/";
 
     @Test
+    @Order(1)
     void createUserTest() {
         //given -config
         //when - http
@@ -33,5 +38,23 @@ public class SmokeApiTests {
                 .when().post()
                 .then().log().body().statusCode(200);
     }
+
+    @Test
+    @Order(2)
+    void checkUserTest() {
+        //given -config
+        //when - http
+        //then - assert
+
+
+        given()
+                .header("accept", "application/json")
+                .header("Content-Type", "application/json")
+                .baseUri(BASE_URL)
+                .basePath("user/string")
+                .when().get()
+                .then().log().body().statusCode(200);
+    }
+
 
 }
